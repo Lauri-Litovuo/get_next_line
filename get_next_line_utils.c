9eaa1998754f5a914442e3d7f215f5f055c6eb41
Lauri Litovuo <llitovuo@c1r1p4.hive.fi>
2023-11-22 14:13:54 +0200

    adding strchr and get_index helper functions to utils

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -3,10 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: llitovuo <llitovuo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: llitovuo <llitovuo@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 09:14:30 by llitovuo          #+#    #+#             */
-/*   Updated: 2023/11/22 09:14:31 by llitovuo         ###   ########.fr       */
+/*   Updated: 2023/11/22 13:59:57 by llitovuo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+char	*ft_strchr(char *s, int c)
+{
+	char	*sp;
+	char	ch;
+
+	sp = (char *)s;
+	ch = c;
+	while (*sp != '\0')
+	{
+		if (*sp == ch)
+			return (sp);
+		sp++;
+	}
+	if (*sp == ch)
+		return (sp);
+	return (NULL);
+}
+
+int	get_index(char *s, int c, int *index)
+{
+	char	*ptr;
+
+	ptr = ft_strchr(s, c);
+	if (ptr == NULL)
+		return (-1);
+	*index = (int)(ptr - s);
+}
