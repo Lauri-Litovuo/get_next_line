27f93ed7a0157a67c9b21679f2a6e01bd351405c
Lauri Litovuo <llitovuo@c1r1p4.hive.fi>
2023-11-22 14:18:21 +0200

    adding prototyping for ft_strchr and get_index to headerfile

--- get_next_line.h
+++ get_next_line.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: llitovuo <llitovuo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: llitovuo <llitovuo@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 09:15:23 by llitovuo          #+#    #+#             */
-/*   Updated: 2023/11/22 09:18:29 by llitovuo         ###   ########.fr       */
+/*   Updated: 2023/11/22 14:15:20 by llitovuo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,7 @@
 # include <stdlib.h>
 
 char	*get_next_line(int fd);
+char	*ft_strchr(char *s, int c);
+int		get_index(char *s, int c, int *index);
 
 #endif
