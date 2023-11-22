edebbec49b45b753a78573c36b95a769771f08fc
Lauri Litovuo <llitovuo@c1r1p4.hive.fi>
2023-11-22 14:12:37 +0200

    skeleton for the general structure for getting the next line to write

--- get_next_line.c
+++ get_next_line.c
@@ -3,16 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: llitovuo <llitovuo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: llitovuo <llitovuo@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 09:13:29 by llitovuo          #+#    #+#             */
-/*   Updated: 2023/11/22 09:18:39 by llitovuo         ###   ########.fr       */
+/*   Updated: 2023/11/22 14:08:28 by llitovuo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
 char	*get_next_line(int fd)
 {
-	char	*str;
+	char			buffer[100];
+	ssize_t			b_read;
+	int				index;
 
-	return (str);
+	index = 0;
+	b_read = read(fd, buffer, sizeof(buffer));
+	if (b_read == -1)
+		return (NULL);
+	if (b_read < sizeof(buffer))
+		buffer[b_read + 1] = '\0';
+	if (get_index(buffer, '\n', &index) < 0)
+		index = sizeof(buffer);
+	return (buffer);
 }
